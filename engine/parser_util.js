efbeefd24fd39aa560d021f35ab2e4d76292e9fc
Robi Lin <xmurobi@gmail.com>
2013-02-21 16:20:05 +0800

    change the regexper for univeral keyvalue match

--- engine/parser_util.js
+++ engine/parser_util.js
@@ -1,37 +1,55 @@
 
 var KeyValueSeparator = {
-	sep : '#@#@',
-	prfx : '#!#!',
+	sep : 		'#@#@',
+
+	// add new pattern for keyvalues extraction here
 	alias : [
-		{sep: ':', prfx: '<li><b>'},
-		{sep: '<\/td><td class="value">', prfx: '<td class="label">'}
+		{sep: ':', pattern: '<li><b>(.(?!<\/li>)|\n)+<\/b>(.(?!<\/b>)|\n)*<\/li>'},
+		{sep: '<\/td><td class="value">', pattern: '<tr[^<>]*><td class="label">.+<\/td><td class="value">.+<\/td><\/tr>'}
 	]
 };
 
 KeyValueSeparator.parseKeyvalues = function (htmlfragment) {
+	var kvs = [], ret = [];
+	var aliasId = -1;
+
+	//htmlfragment = htmlfragment.replace(/\n/ig,' ');
+	//console.log('>>>>>>>>>>\n' + htmlfragment + '\n<<<<<<<<<<<<');
+
 	for (var i = KeyValueSeparator.alias.length - 1; i >= 0; i--) {
 		if (htmlfragment.search(KeyValueSeparator.alias[i].sep) > 0) {
-			htmlfragment = htmlfragment.replace(new RegExp(KeyValueSeparator.alias[i].prfx, 'g'), KeyValueSeparator.prfx);
-			htmlfragment = htmlfragment.replace(new RegExp(KeyValueSeparator.alias[i].sep,'g'), KeyValueSeparator.sep);
+			kvs = htmlfragment.match(new RegExp(KeyValueSeparator.alias[i].pattern, 'g'));
+			aliasId = i;
+			console.log('hit!!!! at:'+i + ' kvs:'+kvs);
 			break;
 		}
 	}
 
-	var kvs = text(htmlfragment).split(KeyValueSeparator.prfx);
-	var ret = [];
-	for (var i = 0; i < kvs.length; i++) {
-		kvs[i] = kvs[i].trim();
-		var kv = kvs[i].split(KeyValueSeparator.sep);
-		if (kv.length == 2) {
-			ret.push({k:kv[0].trim(),v:kv[1].trim()});
+	if (aliasId >= 0) {
+		//console.log('kvs size:'+kvs.length+'\n');
+		for (var i = 0; i < kvs.length; i++) {
+			kvs[i] = kvs[i].replace(/\n/g,'');
+			kvs[i] = kvs[i].replace(new RegExp(KeyValueSeparator.alias[aliasId].sep, 'g'), KeyValueSeparator.sep);
+			kvs[i] = text(kvs[i]);
+			console.log(kvs[i]);
 		}
-	};
+
+		for (var i = 0; i < kvs.length; i++) {
+			kvs[i] = kvs[i].trim();
+			var kv = kvs[i].split(KeyValueSeparator.sep);
+			if (kv.length == 2) {
+				ret.push({k:kv[0].trim(),v:kv[1].trim()});
+			}
+		}
+	}
 
 	return ret;
 }
 
 function text(htmlfragment) {
-	return htmlfragment.replace(/(<([^>]+)>)/ig,'');
+	return htmlfragment
+		.replace(/<script[^<>]*>.*<\/script>|<style[^<>]*>.*<\/style>|&nbsp;/ig,'')
+		.replace(/(<([^>]+)>)/ig,'');
 }
 
 module.exports = KeyValueSeparator;
--- test/ama_item_detail_render_test.js
+++ test/ama_item_detail_render_test.js
@@ -1,25 +1,30 @@
 var should = require("should");
 var fs = require('fs');
-
+_status = require("../global/status")
 var AmaItemDetailRender = require("../engine/ama_item_detail_render");
-//var KVParser = require("../engine/parser_util");
-var r = new AmaItemDetailRender();
+var KVParser = require("../engine/parser_util");
+var crawler = require("../engine/ama_item_detail_crawler")
+var renderer = new AmaItemDetailRender();
 
 describe("ItemRendererTester", function() {
 	it("testing", function(done) {
-		var i1 = fs.readFileSync('./data/i1','UTF-8');
+		// var i1 = fs.readFileSync('./data/i1','UTF-8');
 
-		r.render(i1, function(err, json, ctx){
-			// var kvs = KVParser.parseKeyvalues(json.prd_details);
-			// json.prd_details = {};
-			// for (var i = 0; i < kvs.length; i++) {
-			// 	var k = kvs[i].k.replace(/\s/g,'_').toLowerCase();
-			// 	json.prd_details[k] = kvs[i].v;
-			// };
+		// renderer.render(i1, function(err, json, ctx){
+		// 	console.log(json);
+		// },{asin:1});
 
-			console.log(json);
-		},{asin:1});
-
-		done();
+		crawler.start('B000FTD1HK',function(err, item){
+			console.log(item);
+			done();
+		});
+				
 	});
+
+	// it("testing KVParser", function() {
+	// 	var txt = '<h2>Product Details</h2>\n\n\n  \n\n<div class="disclaim">Platform: <strong>Nintendo Wii</strong></div>\n\n\n\n\n\n\n\n\n\n\n\n    \n      <div class="content">\n\n\n\n\n\n\n\n<ul>\n\n\n\n\n\n\n\n<li><b>Shipping: </b>This item is also available for shipping to select countries outside the U.S.</li>\n\n  \n\n\n<li><b>ASIN:</b> B0088I8M10</li>\n\n\n\n\n\n\n\n\n\n\n\n                                                                                \n\n\n    <li><b>\n    Product Dimensions: \n    </b>\n    7.5 x 5.3 x 0.6 inches ; 2.9 ounces\n    </li>\n\n\n\n<li><b>Media:</b> Video Game</li>\n\n\n\n\n\n\n\n\n      <li><b> Release Date:</b> October 23, 2012</li>'
+	// 	//console.log(txt);
+	// 	KVParser.parseKeyvalues(txt);
+	// 	done();
+	// });
 });
